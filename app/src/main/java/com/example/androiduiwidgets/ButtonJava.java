4d12c98f40381abfc705538a0de2ce27a87fb0c6
Akshat Srivastava <akshatsrivastava566@gmail.com>
2024-03-04 22:36:49 +0530

    This is my third commit

---
+++ app/src/main/java/com/example/androiduiwidgets/ButtonJava.java
@@ -0,0 +1,14 @@
+package com.example.androiduiwidgets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButtonJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_java);
+    }
+}
